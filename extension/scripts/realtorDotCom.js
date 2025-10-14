d50b4f4b41f1dda507bc3c7ebc9d4b190e908a51
Reggie Gilliard <68339917+R-Gilliard-Jr@users.noreply.github.com>
2025-10-14 14:02:17 -0400

    Extension: realtorDotCom
    - Better parsing of realtor.com HTML
    - Run on first mutation only
    - Only process an address if never before seen

--- extension/scripts/realtorDotCom.js
+++ extension/scripts/realtorDotCom.js
@@ -1,41 +1,35 @@
 const cardSelector = "div[data-testid='rdc-property-card']"
-const propertyCharacteristics = ["property-meta-beds", "property-meta-baths", "property-meta-lot-size", "card-price", "card-address"]
+const propertyCharacteristics = ["property-meta-beds", "property-meta-baths", "property-meta-lot-size", "card-price", "card-address-1", "card-address-2"]
+sessionStorage.setItem("processedCards", JSON.stringify([]));
 
 function parseCards() {
     const card_list = document.querySelectorAll(cardSelector);
     const card_array = [...card_list];
     console.log(`ARRAY LENGTH: ${card_array.length}`);
     card_array.forEach((card) => {
-        const cardContent = card.querySelector("div[data-testid='card-content']");
-        const cardData = {};
-        for (i = 0; i < propertyCharacteristics.length; i++) {
-            const characteristic = propertyCharacteristics[i];
-            try {
-                var value = cardContent.querySelector(`div[data-testid=${characteristic}],li[data-testid=${characteristic}]`).textContent;
+        const processedCards = JSON.parse(sessionStorage.getItem("processedCards"));
+        const address = card.querySelector("div[data-testid='card-address']").textContent
+        if (!(processedCards.includes(address))) {
+            const cardContent = card.querySelector("div[data-testid='card-content']");
+            const cardData = {};
+            for (i = 0; i < propertyCharacteristics.length; i++) {
+                const characteristic = propertyCharacteristics[i];
+                try {
+                    var value = cardContent.querySelector(`div[data-testid=${characteristic}], li[data-testid=${characteristic}] span[class='meta-value'], li[data-testid=${characteristic}] span[data-testid='meta-value']`).textContent;
+                }
+                catch {
+                    var value = null;
+                }
+                cardData[characteristic] = value;
             }
-            catch {
-                var value = null;
-            }
-            cardData[characteristic] = value;
+            console.log(cardData);
+            processedCards.push(address);
+            sessionStorage.setItem("processedCards", JSON.stringify(processedCards));
         }
-        console.log(cardData);
     })
 }
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     parseCards();
-// });
-
-parseCards();
-
 const observer = new MutationObserver((mutations) => {
-    // for (const mutation of mutations) {
-    //     for (const node of mutation.addedNodes) {
-    //         if (node instanceof element && node.matches(cardSelector)) {
-    //             parseCards();
-    //         }
-    //     }
-    // }
     parseCards();
 });
 
